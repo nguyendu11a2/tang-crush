0789477504849b5142d08713bb9837249bf28289
nguyendu11a2 <49590692+nguyendu11a2@users.noreply.github.com>
2021-07-01 22:48:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: `Em ăn Riêu Cua, Riêu cá rồi vậy cho anh hỏi em đã ăn Real Love bao giờ chưa???`,
+    btnIntro: 'Chưa :3',
+    title: 'Vậy ăn anh đi =))))))))) 😙',
+    desc: 'ĐI MÒA :3 ',
+    btnYes: 'OK ANH <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +12,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/ThanhDiepBK/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
